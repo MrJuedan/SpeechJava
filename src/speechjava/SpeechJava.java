b0edd78cc68d646deeed7f59d934f3128821c520
julroubens <julroubens2005@gmail.com>
2017-04-30 20:46:32 -0400

    dimention

--- src/speechjava/SpeechJava.java
+++ src/speechjava/SpeechJava.java
@@ -16,8 +16,10 @@ public class SpeechJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
         Fenetre fen = new Fenetre();
         fen.setVisible(true);
+        fet.setSize(new Dimention(600,600));
     }
     
 }
